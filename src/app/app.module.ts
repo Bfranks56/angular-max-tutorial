6480248c7bc493c530a56360741b25c45d52be30
Bryant <bryant.franks@gmail.com>
2020-07-24 15:35:12 -0400

    17-21: working with component styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { ServerComponent } from './server/server.component';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    ServerComponent
     ],
   providers: [],
   bootstrap: [AppComponent]
