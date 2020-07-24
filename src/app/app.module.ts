06753618c176dbd24c6734b1b80c439403ca3b3d
Bryant <bryant.franks@gmail.com>
2020-07-24 15:09:59 -0400

    9-16 Creating a new COmponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServerComponent } from './server/server.component';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
-  ],
+    FormsModule,
+    HttpClientModule,
+    ServerComponent
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
